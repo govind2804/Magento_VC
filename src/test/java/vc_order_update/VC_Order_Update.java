f6780d93610a9d3c2697cea2f7bd5abfdbc27440
govind.tyagi <govind.tyagi@GovindHOB.prhpl.com>
2023-10-06 12:08:12 +0530

    FIRST COMMIT

---
+++ src/test/java/vc_order_update/VC_Order_Update.java
@@ -0,0 +1,62 @@
+package vc_order_update;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class VC_Order_Update {
+
+	@Test
+	public void update() throws Exception {
+		
+		//driver initialization
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		Thread.sleep(2000);
+
+		//initiate url
+		String url = "https://manage.boddess.com/admin/admin/";
+		driver.get(url);
+		Thread.sleep(50000);
+
+		//username
+		WebElement uname = driver.findElement(By.xpath("//*[@id='username']"));
+		uname.clear();
+		uname.sendKeys("govind");
+
+		// pw feed
+		WebElement pw = driver.findElement(By.xpath("//*[@id='login']"));
+		pw.clear();
+		pw.sendKeys("Aspire@1005");
+
+		// click login
+		WebElement login = driver.findElement(By.xpath("//button"));
+		login.click();
+		Thread.sleep(5000);
+
+		// order update loop
+		for (int input = 1305647; input <= 1305737; input = input + 3) {
+			try {
+				String url1 = "https://manage.boddess.com/admin/sales/order/view/order_id/" + input + "/";
+				driver.get(url1);
+				Thread.sleep(5000);
+
+				// find vc tab
+				driver.findElement(By.xpath("//*[@title='Vinculum Order']")).click();
+				Thread.sleep(5000);
+			} catch (Exception e) {
+				System.out.println("Something went wrong.");
+			}
+		}
+
+		// sign out
+		driver.findElement(By.xpath("//span[@class='admin-user-account-text']")).click();
+		driver.findElement(By.xpath("//a[@title='Sign Out']")).click();
+		Thread.sleep(3000);
+		driver.quit();
+	}
+
+}
